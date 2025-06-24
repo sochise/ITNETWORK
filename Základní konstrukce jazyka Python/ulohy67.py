424428c4a56252b3b351dc22ca2b07644e404ecc
sochise <sochise@gmail.com>
2025-06-24 22:16:50 +0200

    pridan python

---
+++ Základní konstrukce jazyka Python/ulohy67.py
@@ -0,0 +1,36 @@
+
+#!/usr/bin/env python3
+
+"""
+print("Zadej jméno: ")
+jmeno = input()
+
+print("Zadej příjmení: ")
+prijmeni = input()
+
+print("Zadej svůj věk: ")
+vek = int(input())
+rok = vek + 1
+
+print(jmeno.upper(), prijmeni.upper())
+print(f"Za rok ti bude {rok} let.")
+
+
+
+print("Zadejte delší slovo: ")
+delsi_slovo = input()
+
+print("Zadejte kratší slovo: ")
+kratsi_slovo = input()
+
+rozdil = len(delsi_slovo) - len(kratsi_slovo)
+
+print(f"Slova se liší délkou o {rozdil} znaků")
+
+"""
+#!/usr/bin/env python3
+
+print("Zadej řetězec: ")
+vstup = input()
+
+print("itnetwork" in vstup.lower())
